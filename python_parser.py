27e6c7dc90f83f474f17e9a07f7e2394683e73a5
Yixin <zhaozicen951230@gmail.com>
2025-08-21 19:50:54 +0200

    ok

---
+++ python_parser.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from collections import deque
+
+class PythonParser:
+    def __init__(self, start_url, max_depth=3):
+        self.start_url = start_url
+        self.max_depth = max_depth
+        self.visited = set()
+
+    def get_links(self, url):
+        links = set() # change list to set
+        try:
+            html = requests.get(url, timeout=5).text
+            soup = BeautifulSoup(html, "html.parser")
+            for a in soup.find_all("a", href=True):
+                full_url = urljoin(url, a["href"])
+                links.add(full_url) # not duplicated
+        except Exception as e:
+            print("Error:", e)
+        return links
+
+    # the output number is the least
+    def python_parser_for_for_for(self):
+        for url1 in self.get_links(self.start_url):
+            if url1 in self.visited:
+                continue
+            self.visited.add(url1)
+            for url2 in self.get_links(url1):
+                if url2 in self.visited:
+                    continue
+                self.visited.add(url2)
+                for url3 in self.get_links(url2):
+                    if url3 in self.visited:
+                        continue
+                    self.visited.add(url3)
+                    print("level 3:", url3)
+
+    #order is different
+    def python_parser_recursion(self, url=None, depth=1):
+        if url is None:
+            url = self.start_url
+        if depth > self.max_depth or url in self.visited:
+            return
+        self.visited.add(url)
+        if depth == self.max_depth:
+            print(f"level {depth}: {url}")
+        else:
+            for link in self.get_links(url):
+                self.python_parser_recursion(link, depth+1)
+
+    def python_parser_bfs(self):
+        queue = deque([(self.start_url, 1)])
+        while queue:
+            url, depth = queue.popleft()
+            if url in self.visited or depth > self.max_depth:
+                continue
+            self.visited.add(url)
+            if depth == self.max_depth:
+                print(f"level {depth}: {url}")
+            else:
+                for new_url in self.get_links(url):
+                    if new_url not in self.visited:
+                        queue.append((new_url, depth+1))
+
+
+if __name__ == "__main__":
+    parser = PythonParser("https://docs.clustervision.com", max_depth=3)
+    # each output is different 
+
+    parser.python_parser_for_for_for() 
+    # parser.python_parser_recursion()
+    # parser.python_parser_bfs()
+
